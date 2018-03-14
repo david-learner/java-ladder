5721abdc3dba2ac3fa3f3b828e4ae7fa25200d0f
David <xodnjs2549@naver.com>
2018-03-14 12:08:53 +0900

    add InputViewUtils - name validation check

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -5,7 +5,8 @@ public class InputView {
 
     public static String[] getNamesOfPersons() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return scanner.nextLine().split(",");
+        String[] namesOfPersons = scanner.nextLine().split(",");
+        return InputViewUtils.checkNames(namesOfPersons);
     }
 
     public static int getHeight() {
